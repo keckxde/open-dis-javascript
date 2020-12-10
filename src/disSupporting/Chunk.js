d538b82cab6002bff9bb9b4db9921baba4ed68b1
Thomas Schneider <Thomas.Rescnt.Schneider@ruag.com>
2020-12-10 23:15:08 +0100

    Renamed initFromBinaryDIS to initFromBinary - as it is called that way by all calling instances, that use the Chunk class

--- src/disSupporting/Chunk.js
+++ src/disSupporting/Chunk.js
@@ -1,28 +1,28 @@
 if (typeof dis === "undefined")
-   dis = {};
+    dis = {};
 
 // Support for node.js style modules; ignore if not using node.js require
 if (typeof exports === "undefined")
-   exports = {};
+    exports = {};
 
 // Replaces (n)ByteChunk functions
 // chunkSize: specify the size of the chunk, ie 1 = 1 byte chunk, 8 = 8 byte chunk, etc.
 // usage: var foo = new Chunk(4) // for a 4 byte chunk
 dis.Chunk = function(chunkSize) {
 
-	data = new Array(chunkSize).fill(0);
-	chunkSize = chunkSize;
+    this.data = new Array(chunkSize).fill(0);
+    this.chunkSize = chunkSize;
 
-	dis.Chunk.prototype.initFromBinaryDIS = function(inputStream) {
-		for(var i = 0; i < this.chunkSize; i++) {
-			this.data[i] = inputStream.readByte();
-		}
-	}
-	dis.Chunk.prototype.encodeToBinaryDIS = function(outputStream) {
-		for(var i = 0; i < this.chunkSize; i++) {
-			outputStream.writeByte(this.data[i]);
-		}
-	}
+    dis.Chunk.prototype.initFromBinary = function(inputStream) {
+        for (var i = 0; i < this.chunkSize; i++) {
+            this.data[i] = inputStream.readByte();
+        }
+    }
+    dis.Chunk.prototype.encodeToBinary = function(outputStream) {
+        for (var i = 0; i < this.chunkSize; i++) {
+            outputStream.writeByte(this.data[i]);
+        }
+    }
 }
 
 exports.Chunk = dis.Chunk;
