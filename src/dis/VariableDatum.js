c41fa867485a336e880ed607c316967059abc168
Thomas Schneider <Thomas.Rescnt.Schneider@ruag.com>
2020-12-10 23:13:55 +0100

    * ArrayLength for VariableDatum has to be divided by 8 - as in the DIS7 implementation

--- src/dis/VariableDatum.js
+++ src/dis/VariableDatum.js
@@ -8,52 +8,52 @@
  */
 // On the client side, support for a  namespace.
 if (typeof dis === "undefined")
- dis = {};
+    dis = {};
 
 
 // Support for node.js style modules. Ignored if used in a client context.
 // See http://howtonode.org/creating-custom-modules
 if (typeof exports === "undefined")
- exports = {};
+    exports = {};
 
 
 dis.VariableDatum = function()
 {
-   /** ID of the variable datum */
-   this.variableDatumID = 0;
+    /** ID of the variable datum */
+    this.variableDatumID = 0;
 
-   /** length of the variable datums, in bits. Note that this is not programmatically tied to the size of the variableData. The variable data field may be 64 bits long but only 16 bits of it could actually be used. */
-   this.variableDatumLength = 0;
+    /** length of the variable datums, in bits. Note that this is not programmatically tied to the size of the variableData. The variable data field may be 64 bits long but only 16 bits of it could actually be used. */
+    this.variableDatumLength = 0;
 
-   /** data can be any length, but must increase in 8 byte quanta. This requires some postprocessing patches. Note that setting the data allocates a new internal array to account for the possibly increased size. The default initial size is 64 bits. */
+    /** data can be any length, but must increase in 8 byte quanta. This requires some postprocessing patches. Note that setting the data allocates a new internal array to account for the possibly increased size. The default initial size is 64 bits. */
     this.variableData = new Array();
- 
+
   dis.VariableDatum.prototype.initFromBinary = function(inputStream)
   {
-       this.variableDatumID = inputStream.readUInt();
-       this.variableDatumLength = inputStream.readUInt();
-       for(var idx = 0; idx < this.variableDatumLength; idx++)
+        this.variableDatumID = inputStream.readUInt();
+        this.variableDatumLength = inputStream.readUInt();
+        for (var idx = 0; idx < this.variableDatumLength / 8; idx++) //take care: the variableDatumLength is in bits (=> divide by 8 for bytes)
        {
-           var anX = new dis.Chunk(1);
-           anX.initFromBinary(inputStream);
-           this.variableData.push(anX);
-       }
+            var anX = new dis.Chunk(1);
+            anX.initFromBinary(inputStream);
+            this.variableData.push(anX);
+        }
 
-  };
+    };
 
   dis.VariableDatum.prototype.encodeToBinary = function(outputStream)
   {
-       outputStream.writeUInt(this.variableDatumID);
-       outputStream.writeUInt(this.variableDatumLength);
+        outputStream.writeUInt(this.variableDatumID);
+        outputStream.writeUInt(this.variableDatumLength);
        for(var idx = 0; idx < this.variableData.length; idx++)
        {
         this.variableData[idx].encodeToBinary(outputStream);
        }
 
-  };
+    };
 }; // end of class
 
- // node.js module support
+// node.js module support
 exports.VariableDatum = dis.VariableDatum;
 
 // End of VariableDatum class
